7653056fe2e91d8d838adf37c8e6fdb46508960c
Jenin82 <jenin8282@gmail.com>
2023-05-05 20:44:25 +0530

    api commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Register } from "./components/Register/Register";
-import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Navigate, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ErrorPage } from "./components/ErrorPage/ErrorPage";
-import Todo from "./components/Todo/Todo";
 import { Login } from "./components/Login/Login";
+import Todo from "./components/Todo/Todo";
 
 function App() {
+	var authUser: boolean = true;
   return (
 		<Router>
 			<Routes>
-				<Route path='/todo' element={<Todo />} />
+				<Route path='/todo' element={authUser? <Todo/>: <Navigate to="/login" />} />
 				<Route path='/register' element={<Register />} />
-				<Route path="/" element={<Navigate to="/login" />}></Route>
+				<Route path="/" element={authUser? <Navigate to="/todo" />: <Navigate to="/login" />}></Route>
 				<Route path='/login' element={<Login />} />
 				<Route path='*' element={<ErrorPage />} />
 			</Routes>
